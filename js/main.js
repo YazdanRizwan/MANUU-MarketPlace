5cde34fd0332646c1f14f187d1c73178d9e64111
Md Yazdan Rizwan <85094744+YazdanRizwan@users.noreply.github.com>
2023-04-16 01:33:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ document.querySelector('.year').innerHTML = date.getFullYear();
 setTimeout(function() {
     $('#message').fadeOut('slow');
 }, 3000);
+
+ sidebarToggle() {
+      if (this.sidebarVisible === false) {
+          this.sidebarOpen();
+      } else {
+          this.sidebarClose();
+      }
+  }
